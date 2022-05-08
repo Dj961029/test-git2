3004ec3bef2fadde567c6bf4115f5285867ecbcb
Dj961029 <1435746538@qq.com>
2022-05-08 22:46:11 +0800

    hot-fix commit 2

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -4,5 +4,6 @@ public class git {
 	public static void main(String[] args) {
 		System.out.println("git1..........success3");
 		System.out.println("git------hot-fix");
+		System.out.println("git------hot-fix2 to 冲突合并");
 	}
 }
