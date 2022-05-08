11eac527a7518e7f69ddc4d2d4484b6eb106eb55
Dj961029 <1435746538@qq.com>
2022-05-08 22:04:43 +0800

    first commit

--- src/main/java/com/atguigu/git.java
+++ src/main/java/com/atguigu/git.java
@@ -2,6 +2,6 @@ package com.atguigu;
 
 public class git {
 	public static void main(String[] args) {
-		System.out.println("git..........success");
+		System.out.println("git1..........success2");
 	}
 }
